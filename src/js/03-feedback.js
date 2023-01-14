46a055f5e33f098940133f878c1adf6cf3c2b3c9
Dennis355 <Snidevichden@gmail.com>
2023-01-15 01:32:50 +0200

    Update 03-feedback.js
    finich

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -20,16 +20,68 @@ function onFormInput (e) {
 
 function onFormSubmit (e) {        
     e.preventDefault();      
-   console.log('Helloy form');
+//    console.log('Helloy form');
+   console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
         e.currentTarget.reset();           
         localStorage.removeItem(STORAGE_KEY);    
        };
  function sddTextToInput () {
 
-    if (localStorage.getItem(STORAGE_KEY)) {
+    if (localStorage.getItem(STORAGE_KEY))  {
 
-         feedbeakForm.email.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).email;
-         feedbeakForm.message.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).message;
-    }
+         feedbeakForm.email.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).email || '';
+         feedbeakForm.message.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).message || '';
+    }  
+    else {  feedbeakForm.email.value = '';
+    feedbeakForm.message.value ='';
+};
 };
        
+
+
+
+
+////////////////  2  //////////////////
+
+// const STORAGE_KEY = "feedbeak-form-state";
+// const feedbeakForm = document.querySelector('.feedback-form');
+
+// const formFeleds = feedbeakForm.elements;
+// console.log(formFeleds);
+
+// const btnSubmit =  feedbeakForm.querySelector('[type="submit"]');
+// console.log(btnSubmit);
+
+// btnSubmit.addEventListener('click', clearLocStor);
+
+// function  clearLocStor () {
+//     localStorage.clear();
+// }
+
+// function attacheEvents ( ) {
+// for (let i = 0; i < formFeleds.length; i++) {
+//     formFeleds[i].addEventListener('change', throttle(changeHandlerForm, 500));
+// }
+//  }
+
+// function  changeHandlerForm (target) {
+   
+//         localStorage.setItem(this.name, this.value);
+//         console.log(this.name, this.value);
+    
+// }
+
+// checkStorage  ();
+
+// function checkStorage  () {
+//     if ( localStorage){ 
+
+    
+//     for (let i = 0; i < formFeleds.length; i++) {
+//     formFeleds[i].value = localStorage.getItem(formFeleds[i].name);
+//     }
+// };
+// };
+
+
+
